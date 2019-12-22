7ab05b5d903e431ac7e8fd17e7401d8805239154
Alexandr Pogodin <awpogodin@mail.ru>
2019-12-22 22:07:48 +0400

    Rename GameFormComponent & remove spec

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {GameCardComponent} from './components/game-card/game-card.component';
-import {GamesAddComponent} from './components/games-add/games-add.component';
+import {GameFormComponent} from './components/game-form/game-form.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
 import {GamesListComponent} from './components/games-list/games-list.component';
@@ -15,7 +15,7 @@ import {GamesSearchComponent} from './components/games-search/games-search.compo
 const routes: Routes = [
   {
     path: 'games/add',
-    component: GamesAddComponent
+    component: GameFormComponent
   },
   {
     path: 'games/:id',
@@ -35,7 +35,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     GameCardComponent,
-    GamesAddComponent,
+    GameFormComponent,
     GamesListComponent,
     HeaderComponent,
     GameEditComponent,
